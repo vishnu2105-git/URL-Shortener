430c8d3da26a2d8a5a280b8c3300e58c12c373e4
Vishnu2118 <128771603+Vishnu2118@users.noreply.github.com>
2025-09-11 15:28:19 +0530

    added profile page

---
+++ frontend/src/Pages/Profile.jsx
@@ -0,0 +1,103 @@
+import React, { use, useEffect } from 'react'
+import { Avatar, Center, Flex, Text } from '@mantine/core';
+import { IconStar } from '@tabler/icons-react';  
+import { Button, Paper } from '@mantine/core';
+
+
+import Service from '../utils/http';
+import { GET_USER_URLS } from '../utils/urls';
+const obj = new Service();
+
+export default function Profile() {
+
+    const[userData,setUserData] = React.useState({});
+
+
+    const getProfileData = async()=>{   
+        try{
+            const data = await obj.get(GET_USER_URLS);
+            setUserData(data);
+            console.log(data);
+        }
+        catch(err){
+            console.log(err);
+        }   
+    }
+    useEffect(()=>{
+
+        getProfileData();
+
+    },[]);
+
+
+  return (
+    <div>
+            <Center style={{display:'flex',flexDirection:'column',gap:'20px',marginTop:'50px'}}>
+            <Paper radius="md" withBorder p="lg" bg="var(--mantine-color-body)">
+            <Avatar
+                src={userData.avatar}
+                size={120}
+                radius={120}
+                mx="auto"
+            />
+            <Text ta="center" fz="lg" fw={500} mt="md">
+                User: {userData.name}
+            </Text>
+            <Text ta="center" c="dimmed" fz="sm">
+                User ID: {userData._id}
+            </Text>
+            </Paper>
+        </Center>
+    </div>
+  )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+{/* <Center style={{display:'flex',flexDirection:'column',gap:'20px',marginTop:'50px'}}>
+            <div>
+                <Avatar variant="filled" radius="md" size="xl" color="cyan" src={userData.avatar} />
+            </div>
+        
+            <div>
+                <Text>User: {userData.name}</Text>
+            </div>
+
+            <div>
+                <Text>User ID: {userData._id}</Text>
+            </div>
+
+            <div>
+                <Text>User Mail: {userData.email}</Text>
+            </div>
+
+        </Center> */}
--- frontend/src/utils/urls.js
+++ frontend/src/utils/urls.js
@@ -1,2 +1,3 @@
 export const GOOGLE_AUTH_LOGIN = 'auth/google'
 export const SHORTEN_URL = 'short-url'
+export const GET_USER_URLS = 'user/me'
